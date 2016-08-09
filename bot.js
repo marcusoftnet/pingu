43ce774f1b8b761028b0625b333684e9a20bb1f9
Marcus Hammarberg <marcusoft.net@gmail.com>
2016-08-09 20:42:29 +0200

    Cleaned up the code

--- bot.js
+++ bot.js
@@ -1,17 +1,24 @@
 const botBuilder = require('claudia-bot-builder');
 const co = require('co');
 const coreq = require('co-request');
+const each = require('co-each');
 
 module.exports = botBuilder(request => {
-    var url = request.text;
+	return co(function* () {
+    	var urls = request.text.split(',');
+    	var messages = yield each(urls, pingUrl)
+    	return messages.join('\n');
+	});
+});
 
-    return co(function*() {
+function pingUrl(url) {
+	return co(function*() {
         var start = new Date();
 	    var result = yield coreq(url);
     	var end = new Date() - start;
 
-        return `Pinged ${url} successfully - took ${end} ms`;
+        return `${url} - ${end} ms`;
     }).catch(function(err) {
 		return `Could not ping ${url} (${err.message})`;
     });
-});
+}
