d9288fd803ebdac0f66a427b2e8cb7ad36cbb031
Marcus Hammarberg <marcusoft.net@gmail.com>
2016-08-10 22:37:36 +0200

    Started implementing with callback

--- bot.js
+++ bot.js
@@ -1,24 +1,58 @@
+'use strict';
+
 const botBuilder = require('claudia-bot-builder');
 const co = require('co');
 const coreq = require('co-request');
 const each = require('co-each');
+const aws = require('aws-sdk');
+const lambda = new aws.Lambda();
+
+const api = botBuilder((message, apiRequest) => {
+
+  return new Promise(
+    (resolve, reject) => {
+      lambda.invoke({
+        FunctionName: apiRequest.lambdaContext.functionName,
+        Qualifier: apiRequest.lambdaContext.functionVersion,
+        InvocationType: 'Event',
+        Payload: JSON.stringify({
+          slackEvent: message
+        })
+      },
+      (err, done) => {
+        if (err) return reject(err);
+          resolve(done);
+        }
+      );
+  }).then(() => {
+    const noOfUrls = message.text.split(',').length;
+
+    return {
+      text: `Wait a sec. I'm pinging ${noOfUrls} urls`
+    }
+  }).catch(() => {
+    return `Ok, something went wrong...`
+  });
+});
 
 module.exports = botBuilder(request => {
-	return co(function* () {
-    	var urls = request.text.split(',');
-    	var messages = yield each(urls, pingUrl)
-    	return messages.join('\n');
-	});
+    return co(function*() {
+        var urls = request.text.split(',');
+        var messages = yield each(urls, pingUrl)
+        return messages.join('\n');
+    });
 });
 
 function pingUrl(url) {
-	return co(function*() {
+    return co(function*() {
         var start = new Date();
-	    var result = yield coreq(url);
-    	var end = new Date() - start;
+        var result = yield coreq(url);
+        var end = new Date() - start;
 
         return `${url} - ${end} ms`;
     }).catch(function(err) {
-		return `Could not ping ${url} (${err.message})`;
+        return `Could not ping ${url} (${err.message})`;
     });
-}
+};
+
+module.exports = api;
