c993286090b83aa7e52475628b7bd0483556be66
Marcus Hammarberg <marcusoft.net@gmail.com>
2016-08-11 20:05:51 +0200

    Updated code for blog post

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
-'use strict';
-
 const botBuilder = require('claudia-bot-builder');
 const co = require('co');
 const coreq = require('co-request');
-const each = require('co-each');
 const aws = require('aws-sdk');
 const lambda = new aws.Lambda();
 
@@ -15,43 +12,61 @@ const api = botBuilder((message, apiRequest) => {
         FunctionName: apiRequest.lambdaContext.functionName,
         Qualifier: apiRequest.lambdaContext.functionVersion,
         InvocationType: 'Event',
-        Payload: JSON.stringify({
-          slackEvent: message
-        })
+        Payload: JSON.stringify({ slackEvent: message })
       },
       (err, done) => {
         if (err) return reject(err);
-          resolve(done);
-        }
-      );
-  }).then(() => {
-    const noOfUrls = message.text.split(',').length;
+        resolve(done);
+      });
+  })
+  .then(() => {
+    const noOfUrls = urlsFromSlackMessage(message).length;
 
     return {
-      text: `Wait a sec. I'm pinging ${noOfUrls} urls`
+      text: `Hold on. I'm pinging ${noOfUrls} urls`
     }
-  }).catch(() => {
-    return `Ok, something went wrong...`
+  })
+  .catch(() => {
+    return `Ok, something went seriously wrong...`
   });
 });
 
-module.exports = botBuilder(request => {
-    return co(function*() {
-        var urls = request.text.split(',');
-        var messages = yield each(urls, pingUrl)
-        return messages.join('\n');
-    });
+
+const slackDelayedReply = botBuilder.slackDelayedReply;
+
+api.intercept((event) => {
+  if (!event.slackEvent) return event;
+
+  const message = event.slackEvent; //original slack message sent to bot
+  const urls = urlsFromSlackMessage(message);
+
+  var pingResults = [];
+  for (const i = 0; i < urls.length; i++) {
+    pingResults.push(pingUrl(urls[i]))
+  }
+
+  return slackDelayedReply(message, {
+    "response_type": "ephemeral",
+    "text": `Ping results for '${message}':`,
+    "attachments": JSON.stringify(pingResults)
+  })
+  .then(() => false); // prevents normal execution
 });
+
+function urlsFromSlackMessage(message){
+  return message.text.split(',');
+};
 
 function pingUrl(url) {
     return co(function*() {
-        var start = new Date();
-        var result = yield coreq(url);
-        var end = new Date() - start;
+      const start = new Date();
+      const result = yield coreq(url);
+      const end = new Date() - start;
 
-        return `${url} - ${end} ms`;
-    }).catch(function(err) {
-        return `Could not ping ${url} (${err.message})`;
+      return `${url} - ${end} ms`;
+    })
+    .catch(function(err) {
+      return `Could not ping ${url} '${err.message}'`;
     });
 };
 
